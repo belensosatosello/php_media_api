288f9c8ca7db6ddf22f082a4c260df3d839529e5
belensosatosello <belen.sosa@hpe.com>
2017-03-05 23:32:36 -0300

    Added PHP Library for Instagram API.

--- app/ApiController.php
+++ app/ApiController.php
@@ -1,48 +1,133 @@
 <?php
-namespace App;
 
+namespace App;
+	
 use Silex\Application;
+use Haridarshan\Instagram\Instagram;
+use Haridarshan\Instagram\InstagramRequest;
+use Haridarshan\Instagram\Exceptions\InstagramOAuthException;
+use Haridarshan\Instagram\Exceptions\InstagramResponseException;
+use Haridarshan\Instagram\Exceptions\InstagramServerException;
 	
 class ApiController
 {
 	public function getMediaLocation(Application $app,$media_id)
-	{
-		//Sample Media ID
-		//$media_id = '1402451097368744018_1173952339';
+	{		
+		$instagram = $app['instagram'];
+		$token = $app['session']->get('token');
 		
-		//Define access_token
-		$access_token='1173952339.4490422.343e3f74bc6a48e9ac3541784d5af621';
-		
-		
-		$instagram_url ="http://api.instagram.com/v1/media/".$media_id."?access_token=".$access_token;
+		try {
+			// To get User Profile Details or to make any api call to instagram
+			$user = new InstagramRequest($instagram, "/media/".$media_id, [ "access_token" => $token ]);
+			// To get Response
+			$user_response = $user->getResponse();
 			
-		//Get the information from instagram
-		$media_json = file_get_contents($instagram_url);
+			// To get Body
+			$user_data = $user_response->getBody();
 			
-		if(!empty($media_json))
+			//If the response was not successful, return the error code
+			if($user_data->meta->code != 200)
+			{
+				return $app->json($response,$response->meta->code);
+			}
+
+		} catch(InstagramResponseException $e) {
+			//return $app->json($e->getMessage());
+			return $app->json($e);
+		}catch(InstagramServerException $e) {
+			return $app->json($e->getMessage());
+		}
+	
+				
+		//Get the location information
+		$lat = $user_data->data->location->latitude;
+		$long =$user_data->data->location->longitude;
+			
+		if(empty($lat) || empty($long))
 		{
-			//Turn this information into a PHP array
-			$media_array = json_decode($media_json, true);
-			
-			//Get the location information
-			$lat = $media_array['data']['location']['latitude'];
-			$long = $media_array['data']['location']['longitude'];
-			
-			$geopoint = array(
+			throw new Exception("No instagram location data was found.");
+		}		
+		
+		$geopoint = array(
 			"geopoint" => array(
 			"latitude" => $lat,
 			"longitude"=> $long,
+			//"code"=> $code,
 			)
-			);
+		);
 			
-			$result_array = array(
+		$result_array = array(
 			"id" => $media_id,
-			"location" => $geopoint
-			);
-			
-		}
-			
+			"location" => $geopoint,
+		);
+		
 		return $app->json($result_array);
-	}		
-}
-?>
+	
+	}
+	
+		
+	/**
+	*	This function gets the token required to retrieve data from the Instagram API.
+	*	
+	*	@param object $app the silex application object.
+	*	
+	* 	@return redirects to /profile.
+	*
+	*	@access public
+	*/
+	public function setToken(Application $app)
+	{
+		$app['session']->remove('token');
+		$instagram = $app['instagram'];
+			
+		// If we don't have an authorization code then get one
+		if (!isset($_GET['code'])) {
+			$scope = [
+				"basic",
+				"public_content"
+			];
+			header('Location: ' . $instagram->getLoginUrl(["scope" => $scope]));
+			exit;
+		} else {
+			$oauth= $instagram->oauth($_GET['code']);
+			$token = $oauth->getAccessToken();
+			$app['session']->set('token', $token);
+			$app['session']->set('oauth', $oauth);
+			return $app->redirect('/profile');
+		}
+	}
+		
+	/**
+	*	This function shows basic information of the user that has been authenticated.
+	*
+	* 	@param object $app the silex application object.
+	*	
+	*	@return string the json response with user data.
+	*
+	*	@access public
+	*/
+	public function getUser(Application $app)
+	{
+		$instagram = $app['instagram'];
+		$token = $app['session']->get('token');
+		
+		try {
+			// To get User Profile Details or to make any api call to instagram
+			$request = new InstagramRequest($instagram, "/users/self", [ "access_token" => $token ]);
+			// To get Response
+			$user_response = $request->getResponse();
+			// To get Body
+			$user_data = $user_response->getData();
+			
+			$user = [];
+			$user['full_name'] = $user_data->full_name;
+			$user['username'] = $user_data->username;
+		
+		} catch(InstagramResponseException $e) {
+			echo $e->getMessage();
+		}catch(InstagramServerException $e) {
+			echo $e->getMessage();
+		}	
+			return $app->json($user);
+		}
+	}
--- web/index.php
+++ web/index.php
@@ -1,25 +1,46 @@
 <?php
-	use App\ApiController;
-	/**
-		* handle static requests
-	*/
-	if (array_key_exists('REQUEST_URI', $_SERVER) &&
-    preg_match('/\.(?:html|js|css|png|jpg|jpeg|gif|woff|ttf)$/', $_SERVER['REQUEST_URI'])) {
-		return false;
-	}
+use App\ApiController;
+use Haridarshan\Instagram\Instagram;
+use Haridarshan\Instagram\InstagramRequest;
+use Haridarshan\Instagram\Exceptions\InstagramOAuthException;
+use Haridarshan\Instagram\Exceptions\InstagramResponseException;
+use Haridarshan\Instagram\Exceptions\InstagramServerException;
 	
-	// web/index.php
-	require_once __DIR__.'/../vendor/autoload.php';
 	
-	$app = new Silex\Application();
+/**
+	* handle static requests
+*/
+if (array_key_exists('REQUEST_URI', $_SERVER) &&
+	preg_match('/\.(?:html|js|css|png|jpg|jpeg|gif|woff|ttf)$/', $_SERVER['REQUEST_URI'])) {
+	return false;
+}
 	
-	//Enable debug to see error details
-	$app['debug'] = true;
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
 	
-	// ... definitions
-	$app-> get('/media/{media_id}', "App\ApiController::getMediaLocation");
+$app = new Silex\Application();
+$app->register(new Silex\Provider\SessionServiceProvider());
 	
-	//Run silex
-	$app->run();
+//Enable debug to see error details
+$app['debug'] = true;
+	
+$app['instagram'] = function() use ($app){
+	return new Instagram(
+		array(
+		'ClientId' => '44904229b57445f49a88ef2de046379f',
+		'ClientSecret' => 'b4b4fc1b7a1540ac91c72eeeca6c3d47',
+		'Callback' => 'http://localhost:8000/',
+		)
+	);
+};
 
-?>
+// ... definitions
+$app-> get('/media/{media_id}', "App\ApiController::getMediaLocation");
+
+// ... definitions
+$app-> get('/', "App\ApiController::setToken");
+
+$app-> get('/profile', "App\ApiController::getUser");
+	
+//Run silex
+$app->run();
